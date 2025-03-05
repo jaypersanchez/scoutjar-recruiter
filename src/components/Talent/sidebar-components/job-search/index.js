30eba71d32b1fecacf13b3d68d994e6419d5272f
Jayper Sanchez <jaypersanchez@gmail.com>
2025-03-06 05:14:07 +0800

    talent pages styled

---
+++ src/components/Talent/sidebar-components/job-search/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../../App.css'; // Corrected path to App.css
+
+function SearchForJob() {
+  return (
+    <div>
+      <h2>Search for Job</h2>
+      <p>Job search functionality goes here.</p>
+    </div>
+  );
+}
+
+export default SearchForJob;
