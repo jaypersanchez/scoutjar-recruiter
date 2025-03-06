d7ae0588724d6a8d79b498c4407a171d5ff8e15a
Jayper Sanchez <jaypersanchez.gmail.com>
2025-03-07 06:59:14 +0800

    saving minor changes

--- src/components/TalentScout/RecruiterSidebar.js
+++ src/components/TalentScout/RecruiterSidebar.js
@@ -2,24 +2,26 @@ import React from 'react';
 import '../../App.css';
 
 function RecruiterSidebar({ setSelectedSection }) {
+  const items = [
+    "Scout for Talent",
+    "Post a Job",
+    "Scout's Job Posts",
+    "Review Talent Applicants",
+    "ScoutJar Talents",
+    "Schedule Interview",
+    "Interview Candidates",
+    "Evaluate Candidates",
+    "Send Job Offer",
+    "Reject Candidates",
+    "Finalize Hiring",
+    "Onboard Candidate"
+  ];
+
   return (
     <aside className="recruiter-sidebar">
       <ul>
-        {[
-          "Scout for Talent",
-          "Post a Job",
-          "My Job Posts",
-          "Review Candidates",
-          "Shortlist Candidates",
-          "Schedule Interview",
-          "Interview Candidates",
-          "Evaluate Candidates",
-          "Send Job Offer",
-          "Reject Candidates",
-          "Finalize Hiring",
-          "Onboard Candidate"
-        ].map((item) => (
-          <li key={item}>
+        {items.map((item, index) => (
+          <li key={item} style={{ display: index < 5 ? 'list-item' : 'none' }}>
             <button className="sidebar-button" onClick={() => setSelectedSection(item)}>
               {item}
             </button>
