db245b377f4b72eded572fa63de70ec222ed2f4a
rgyogiii <romar.doinog@gmail.com>
2025-03-10 11:18:21 +0800

    fix folder names

--- src/pages/routes/AuthRoutes.tsx
+++ src/pages/routes/AuthRoutes.tsx
@@ -1,7 +1,7 @@
 import { PageRouteProps } from "@/common/types/routes.types";
 import { AuthLayout } from "@/common/components/layouts";
 
-import LoginPage from "@/pages/Auth/Login";
+import LoginPage from "@/pages/access/Login";
 
 const AUTH_ROUTES: Array<PageRouteProps> = [
   {
