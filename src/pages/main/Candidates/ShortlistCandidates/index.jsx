39156f5bad90454275c10b21e5a0cec5d4eea730
jaypersanchez <jaypersanchez@gmail.com>
2025-04-10 12:15:11 +0800

    Styling changes and logo to LooKK icon and using color theme based on the image colors

--- src/pages/main/Candidates/ShortlistCandidates/index.jsx
+++ src/pages/main/Candidates/ShortlistCandidates/index.jsx
@@ -105,7 +105,8 @@ export default function ShortlistedCandidates() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="p-4 recruiter-content">
+
       <h2 className="text-2xl font-bold mb-4 uppercase text-center text-primary">
         Shortlisted Candidates
       </h2>
