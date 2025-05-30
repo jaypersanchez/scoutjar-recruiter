6ba8e8dffe8a4bd4c4c0d06b26b5afd695e61d01
jaypersanchez <jaypersanchez@gmail.com>
2025-05-30 08:47:49 +0800

    changed default image for profile

--- src/pages/main/Users/Profile/components/ProfileForm.jsx
+++ src/pages/main/Users/Profile/components/ProfileForm.jsx
@@ -155,7 +155,7 @@ export default function ProfileForm() {
         ? previewUrl
         : profileData.company_logo
         ? `${baseUrl}${profileData.company_logo}`
-        : "/logo.png"
+        : "/lookk.png"
     }
     alt="Company Logo"
     onClick={handleLogoClick}
